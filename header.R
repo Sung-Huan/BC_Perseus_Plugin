416a2de6af079aeb98cf19911cd1f0ab9345ac30
Yu <shyu@w2k.biochem.mpg.de>
2019-07-24 01:32:01 +0200

    add header.R

---
+++ header.R
@@ -0,0 +1,16 @@
+args = commandArgs(trailingOnly = TRUE)
+
+if (length(args) != 3) {
+    stop("Should provide two arguments: paramFile inFile outFile", call. = FALSE)
+}
+paramFile <- args[1]
+inFile <- args[2]
+outFile <- args[3]
+library(PerseusR)
+parameters <- parseParameters(paramFile)
+num_row <- intParamValue(parameters, 'Number of rows')
+mdata <- read.perseus(inFile)
+counts <- main(mdata)
+mdata2 <- head(counts, n=num_row)
+print(paste('writing to', outFile))
+write.perseus(mdata2, outFile)
