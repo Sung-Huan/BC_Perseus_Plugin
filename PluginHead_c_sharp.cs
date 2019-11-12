e6c4d94d9dabe8207035c912eb04c359b3de6e33
Sung-Huan (Silas) Yu <sung-huan.yu@uni-wuerzburg.de>
2019-11-12 13:58:05 +0100

    add C file and project structure

---
+++ PluginHead_c_sharp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using BaseLibS.Num;
+using BaseLibS.Graph;
+using BaseLibS.Param;
+using PerseusApi.Document;
+using PerseusApi.Generic;
+using PerseusApi.Matrix;
+using System.Collections.Generic;
+
+namespace PluginHead
+{
+	public class PluginHead : IMatrixProcessing
+	{
+		public bool HasButton => false;
+		public string Description => "extract the header of the matrix.";
+		public string HelpOutput => "extract the header of the matrix.";
+		public string[] HelpSupplTables => new string[0];
+		public int NumSupplTables => 0;
+		public string Name => "Header_CS_only";
+		public string Heading => "Tutorial";
+		public float DisplayRank => 6;
+		public string[] HelpDocuments => new string[0];
+		public int NumDocuments => 0;
+		public string Url => null;
+		public Bitmap2 DisplayImage => null;
+		public bool IsActive => true;
+
+		public int GetMaxThreads(Parameters parameters)
+		{
+			return 1;
+		}
+
+		public void ProcessData(IMatrixData mdata, Parameters param, ref IMatrixData[] supplTables,
+			ref IDocumentData[] documents, ProcessInfo processInfo)
+		{
+			int lines = param.GetParam<int>("Number of rows").Value;
+			int[] remains = Enumerable.Range(0, lines).ToArray();
+			mdata.ExtractRows(remains);
+		}
+
+		public Parameters GetParameters(IMatrixData mdata, ref string errorString)
+		{
+			return new Parameters(new IntParam("Number of rows", 15)
+			{
+				Help = "The number of rows for the header needs to be kept."
+			});
+		}
+	}
+}
---
+++ PluginHead_with_resource.cs
@@ -0,0 +1,50 @@
+﻿using BaseLibS.Param;
+using PerseusApi.Matrix;
+using System.IO;
+using PluginInterop;
+using System.Text;
+using PluginHead.Properties; // replace PluginHead3 to your project or solution name
+
+namespace PluginHead
+{
+    public class Head : PluginInterop.Python.MatrixProcessing // if you use R, replace Python to R
+    {
+        public override string Heading => "Tutorial";
+        public override string Name => "Header_with_resource";
+        public override string Description => "extract the header of the matrix";
+        public override bool IsActive => true;
+        public override string Url => null;
+
+        protected override bool TryGetCodeFile(Parameters param, out string codeFile)
+        {
+            byte[] code = (byte[])Resources.ResourceManager.GetObject("header_c_sharpPy"); // put the script to resources via edit properies file, and replace header_c_sharpPy to your script name
+            codeFile = Path.GetTempFileName();
+            File.WriteAllText(codeFile, Encoding.UTF8.GetString(code));
+            return true;
+        }
+
+        protected override string GetCommandLineArguments(Parameters param)
+        {
+            var tempFile = Path.GetTempFileName();
+            param.ToFile(tempFile);
+            return tempFile;
+        }
+
+        protected override Parameter[] SpecificParameters(IMatrixData mdata, ref string errString)
+        {
+
+            if (mdata.ColumnCount < 3)
+            {
+                errString = "Please add at least 3 main data columns to the matrix.";
+                return null;
+            }
+            return new Parameter[]
+            {
+                new IntParam("Number of rows", 15)
+                {
+                    Help = "The number of rows for the header needs to be kept."
+                }
+            };
+        }
+    }
+}
